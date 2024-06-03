818aabc2fc2cdf76285adb5397b327937ddf7729
Manojajj <144515384+Manojajj@users.noreply.github.com>
2024-06-03 19:46:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ def main():
     st.title("Gold Price Prediction")
 
     # Load the saved model
-    with open('gold_price_model.pkl', 'rb') as f:
-        model = pickle.load(f)
+    with open('gold_price_model.pkl', 'rb') as file:
+        model = pickle.load(file)
 
     # Sidebar for user inputs
     st.sidebar.title("Gold Price Prediction Inputs")
-    SPX = st.sidebar.number_input("SPX")
-    USO = st.sidebar.number_input("USO")
-    SLV = st.sidebar.number_input("SLV")
-    EUR_USD = st.sidebar.number_input("EUR/USD")
-    day = st.sidebar.selectbox("Day", list(range(1, 8)))
+    day = st.sidebar.selectbox("Day", list(range(1, 32)))
     month = st.sidebar.selectbox("Month", list(range(1, 13)))
+    spx = st.sidebar.number_input("S&P 500 (SPX)")
+    uso = st.sidebar.number_input("US Oil Rate (USO)")
+    slv = st.sidebar.number_input("Silver Rate (SLV)")
+    eurusd = st.sidebar.number_input("EUR/USD Exchange Rate")
 
     # Make prediction
     if st.sidebar.button("Predict"):
-        input_data = pd.DataFrame([[SPX, USO, SLV, EUR_USD, day, month]], columns=['SPX', 'USO', 'SLV', 'EUR/USD', 'Day', 'Month'])
+        input_data = pd.DataFrame([[spx, uso, slv, eurusd, day, month]], columns=['SPX', 'USO', 'SLV', 'EUR/USD', 'Day', 'Month'])
         prediction = model.predict(input_data)
         st.write(f"Predicted Gold Price: {prediction[0]}")
 
